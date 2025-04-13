07aa47aef6577a548dc204db3a2ef7cb28ace32a
Linus <linuskorir20@gmail.com>
2025-04-13 23:48:33 +0300

    fix: update initial route to default home screen

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: AppConstants.appName,
       theme: AppTheme.lightTheme, 
-      initialRoute: '/dashboard',
+      initialRoute: '/',
       onGenerateRoute: AppRoutes.generateRoute,
     );
   }
